2885c09aedd4054e80a015f86700e61ebba632bb
Julius Kiefer <juliuskiefer@web.de>
2022-06-20 02:03:52 +0200

    changed logic to continually draw lines

--- art_with_ai/main.py
+++ art_with_ai/main.py
@@ -1,17 +1,23 @@
 __author__ = 'moejitow'
 
 import sys
+import time
 import pygame
 from pygame.locals import QUIT
 from art_with_ai import display
+from art_with_ai import image
 
 
 def main():
     display.init_screen()
 
-    display.draw_lines()
-
     while True:
+
+        image.take_image()
+        landmarks = image.collect_landmarks()
+        display.draw_lines(landmarks)
+        time.sleep(1)
+
         for events in pygame.event.get():
             if events.type == QUIT:
                 sys.exit(0)
